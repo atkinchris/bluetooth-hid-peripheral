c5824725387952a860405f875dffb5276ef09606
Chris Atkin <atkinchris@gmail.com>
2018-06-12 21:48:29 +0100

    Add Generic Access service

---
+++ bluetooth-hid-peripheral/Constants.swift
@@ -0,0 +1,8 @@
+import CoreBluetooth
+
+struct Constants {
+    static let HID_SERVICE_UUID = CBUUID(string: "1812")
+    static let GENERIC_ACCESS_UUID = CBUUID(string: "1800")
+    static let APPEARANCE_UUID = CBUUID(string: "2A01")
+    static let DEVICE_NAME_UUID = CBUUID(string: "2A00")
+}
--- bluetooth-hid-peripheral/ViewController.swift
+++ bluetooth-hid-peripheral/ViewController.swift
@@ -11,13 +11,37 @@ class ViewController: NSViewController {
         super.viewDidLoad()
         status.isEditable = false
         peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
+        
+
     }
 }
 
 extension ViewController : CBPeripheralManagerDelegate {
+    func registerServices(_ peripheral: CBPeripheralManager) {
+        let genericAccess = CBMutableService(type: Constants.GENERIC_ACCESS_UUID, primary: true)
+        let deviceName = CBMutableCharacteristic(
+            type: Constants.DEVICE_NAME_UUID,
+            properties: CBCharacteristicProperties.read,
+            value: "BLE Keyboard".data(using: .utf8),
+            permissions: CBAttributePermissions.readable
+        )
+        let appearance = CBMutableCharacteristic(
+            type: Constants.APPEARANCE_UUID,
+            properties: CBCharacteristicProperties.read,
+            value: Data(bytes: [9, 61]),
+            permissions: CBAttributePermissions.readable
+        )
+        genericAccess.characteristics = [deviceName, appearance]
+        peripheral.add(genericAccess)
+        
+        let hidService = CBMutableService(type: Constants.HID_SERVICE_UUID, primary: true)
+        peripheral.add(hidService)
+    }
+    
     func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
         if (peripheral.state == .poweredOn && !peripheralManager.isAdvertising) {
             status.stringValue = "Ready!"
+            registerServices(peripheral)
             peripheralManager.startAdvertising([
                 CBAdvertisementDataLocalNameKey:"BLE Keyboard",
                 CBAdvertisementDataServiceUUIDsKey: uuid,
