7f818738c01c45b60b19d629a94fb3c46b9a12f7
Chris Atkin <atkinchris@gmail.com>
2018-06-12 21:06:24 +0100

    Prevent advertising more than once

--- bluetooth-hid-peripheral/ViewController.swift
+++ bluetooth-hid-peripheral/ViewController.swift
@@ -2,32 +2,34 @@ import Cocoa
 import CoreBluetooth
 
 class ViewController: NSViewController {
-    @IBOutlet var label: NSView!
+    @IBOutlet weak var status: NSTextField!    
     
-    var peripheralManager:CBPeripheralManager?
+    var peripheralManager = CBPeripheralManager()
     let uuid:CBUUID = CBUUID(string: "DCEF54A2-31EB-467F-AF8E-350FB641C97B")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
+        status.isEditable = false
+        peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
     }
 }
 
 extension ViewController : CBPeripheralManagerDelegate {
     func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
-        if (peripheral.state == .unsupported) {
-            print("unsupported")
+        if (peripheral.state == .poweredOn && !peripheralManager.isAdvertising) {
+            status.stringValue = "Ready!"
+            peripheralManager.startAdvertising([
+                CBAdvertisementDataLocalNameKey:"BLE Keyboard",
+                CBAdvertisementDataServiceUUIDsKey: uuid,
+            ])
         }
         
-        if (peripheral.state == .poweredOn) {
-            label.insertText("self.peripheralManager powered on.")
-            print("POWER ON")
-            peripheralManager?.startAdvertising([CBAdvertisementDataLocalNameKey:"my-peripheral", CBAdvertisementDataServiceUUIDsKey: uuid])
+        if (peripheral.state == .poweredOff && peripheralManager.isAdvertising) {
+            peripheralManager.stopAdvertising()
         }
     }
     
     func peripheralManagerDidStartAdvertising(_ peripheral: CBPeripheralManager, error: Error?) {
         print("started advertising")
-        print(peripheral)
     }
 }
