c2c33a561475fc2ef792601ec234333d2ccb9115
Chris Atkin <atkinchris@gmail.com>
2018-06-13 09:04:13 +0100

    Use service UUID for peripheral

--- bluetooth-hid-peripheral/ViewController.swift
+++ bluetooth-hid-peripheral/ViewController.swift
@@ -5,50 +5,37 @@ class ViewController: NSViewController {
     @IBOutlet weak var status: NSTextField!    
     
     var peripheralManager = CBPeripheralManager()
-    let uuid:CBUUID = CBUUID(string: "DCEF54A2-31EB-467F-AF8E-350FB641C97B")
+    
+    let hidService = CBMutableService(type: Constants.HID_SERVICE_UUID, primary: true)
+    let genericAccess = CBMutableService(type: Constants.GENERIC_ACCESS_UUID, primary: true)
+    let deviceName = CBMutableCharacteristic(type: Constants.DEVICE_NAME_UUID, properties: .read, value: "BLE Keyboard".data(using: .utf8), permissions: .readable)
+    let appearance = CBMutableCharacteristic(type: Constants.APPEARANCE_UUID, properties: .read, value: Data(bytes: [9, 61]), permissions: .readable)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         status.isEditable = false
         peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
-        
-
     }
 }
 
 extension ViewController : CBPeripheralManagerDelegate {
     func registerServices(_ peripheral: CBPeripheralManager) {
-        let genericAccess = CBMutableService(type: Constants.GENERIC_ACCESS_UUID, primary: true)
-        let deviceName = CBMutableCharacteristic(
-            type: Constants.DEVICE_NAME_UUID,
-            properties: CBCharacteristicProperties.read,
-            value: "BLE Keyboard".data(using: .utf8),
-            permissions: CBAttributePermissions.readable
-        )
-        let appearance = CBMutableCharacteristic(
-            type: Constants.APPEARANCE_UUID,
-            properties: CBCharacteristicProperties.read,
-            value: Data(bytes: [9, 61]),
-            permissions: CBAttributePermissions.readable
-        )
         genericAccess.characteristics = [deviceName, appearance]
         peripheral.add(genericAccess)
-        
-        let hidService = CBMutableService(type: Constants.HID_SERVICE_UUID, primary: true)
         peripheral.add(hidService)
     }
     
     func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
-        if (peripheral.state == .poweredOn && !peripheralManager.isAdvertising) {
+        if (peripheral.state == .poweredOn && !peripheral.isAdvertising) {
             status.stringValue = "Ready!"
             registerServices(peripheral)
-            peripheralManager.startAdvertising([
+            peripheral.startAdvertising([
                 CBAdvertisementDataLocalNameKey:"BLE Keyboard",
-                CBAdvertisementDataServiceUUIDsKey: uuid,
+                CBAdvertisementDataServiceUUIDsKey: hidService.uuid,
             ])
         }
         
-        if (peripheral.state == .poweredOff && peripheralManager.isAdvertising) {
+        if (peripheral.state == .poweredOff && peripheral.isAdvertising) {
             peripheralManager.stopAdvertising()
         }
     }
